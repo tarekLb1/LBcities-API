9d324d2c3cc2848caff532b5017d745c898b31af
tarekbaarini <tarekbaarini02@gmail.com>
2023-02-24 17:09:05 -0800

    3rd commit Remove CORS

--- api.js
+++ api.js
@@ -4,9 +4,9 @@ const cors = require('cors')
 const LBcities = require('./LBcities.json');
 const PORT = process.env.PORT || 3030;
 
-app.use(cors({
-  origin: 'http://127.0.0.1:5500'
-}));
+// app.use(cors({
+//   origin: 'http://127.0.0.1:5500'
+// }));
 
 app.get('/LBcities', function (req, res) {
   res.send(LBcities)
