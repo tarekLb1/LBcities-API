37137a59cb735cc2afd02e0490ce496d33e84494
tarekbaarini <tarekbaarini02@gmail.com>
2023-02-24 17:00:54 -0800

    Add PORT

--- api.js
+++ api.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors')
 const LBcities = require('./LBcities.json');
-
+const PORT = process.env.PORT || 3030;
 
 app.use(cors({
   origin: 'http://127.0.0.1:5500'
@@ -13,4 +13,4 @@ app.get('/LBcities', function (req, res) {
 })
 
 
-app.listen(3000)
+app.listen(PORT)
