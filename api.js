ad5821e0e7b93445b3fcbd88cc3e1d7f00de828e
tarekbaarini <tarekbaarini02@gmail.com>
2023-02-25 21:12:33 -0800

    test11

--- api.js
+++ api.js
@@ -5,7 +5,7 @@ const LBcities = require('./LBcities.json');
 const PORT = process.env.PORT || 3030;
 
  app.use(cors({
-   origin: 'http://127.0.0.1:5500'
+   origin: 'http://127.0.0.1:5502'
  }));
 
 app.get('/LBcities', function (req, res) {
