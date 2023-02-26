845a075de9754311b7e6dcef43c12e47d1b6efc6
tarekbaarini <tarekbaarini02@gmail.com>
2023-02-26 13:34:29 -0800

    added the modified LBcitiesmodified.json

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors')
-const LBcities = require('./LBcities.json');
+const LBcities = require('./LBcitiesmodified.json');
 const PORT = process.env.PORT || 3030;
 
  app.use(cors({
